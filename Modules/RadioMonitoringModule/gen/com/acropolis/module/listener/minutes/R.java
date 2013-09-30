5204ba4a6cee7fa3c7b04b3fb589457efc0c2025
StillNotThereYet <rohan.mahendroo@gmail.com>
2013-09-30 18:35:20 -0400

    Ongoing RadioMonitoringModule
    Interface frame added

--- Modules/RadioMonitoringModule/gen/com/acropolis/module/listener/minutes/R.java
+++ Modules/RadioMonitoringModule/gen/com/acropolis/module/listener/minutes/R.java
@@ -28,7 +28,36 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08001d;
+        public static final int bttnRefresh=0x7f08001c;
+        public static final int dataGrid=0x7f080014;
+        public static final int msgGrid=0x7f08000b;
+        public static final int relativeLayout=0x7f080000;
+        public static final int space1=0x7f080009;
+        public static final int space2=0x7f080012;
+        public static final int space3=0x7f08001b;
+        public static final int strData=0x7f080013;
+        public static final int strMsg=0x7f08000a;
+        public static final int strVoice=0x7f080001;
+        public static final int txtDTotal=0x7f080019;
+        public static final int txtDown=0x7f080015;
+        public static final int txtIn=0x7f080003;
+        public static final int txtMTotal=0x7f080010;
+        public static final int txtOut=0x7f080005;
+        public static final int txtRcv=0x7f08000c;
+        public static final int txtSnt=0x7f08000e;
+        public static final int txtUpload=0x7f080017;
+        public static final int txtVTotal=0x7f080007;
+        public static final int valueDTotal=0x7f08001a;
+        public static final int valueDown=0x7f080016;
+        public static final int valueIn=0x7f080004;
+        public static final int valueMTotal=0x7f080011;
+        public static final int valueOut=0x7f080006;
+        public static final int valueRcv=0x7f08000d;
+        public static final int valueSnt=0x7f08000f;
+        public static final int valueUp=0x7f080018;
+        public static final int valueVTotal=0x7f080008;
+        public static final int voiceGrid=0x7f080002;
     }
     public static final class layout {
         public static final int activity_minutes_module=0x7f030000;
@@ -39,22 +68,27 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int callTypeString=0x7f050005;
+        public static final int btnstrRefresh=0x7f050005;
+        /**  Standard constants 
+         */
+        public static final int fetching=0x7f050003;
         public static final int screenHeading=0x7f050002;
         /**  Voice Call 
          */
-        public static final int strCallHeading=0x7f050003;
+        public static final int strCallHeading=0x7f050006;
         /**  Data  
          */
-        public static final int strDataHeading=0x7f050009;
-        public static final int strDownloadedData=0x7f05000a;
-        public static final int strIncomingCallSeconds=0x7f050004;
+        public static final int strDataHeading=0x7f05000c;
+        public static final int strDownloaded=0x7f05000d;
+        public static final int strIncoming=0x7f050007;
         /**  Messaging 
          */
-        public static final int strMessageHeading=0x7f050006;
-        public static final int strReceivedMessages=0x7f050007;
-        public static final int strSentMessages=0x7f050008;
-        public static final int strUploadedData=0x7f05000b;
+        public static final int strMessageHeading=0x7f050009;
+        public static final int strOutgoing=0x7f050008;
+        public static final int strReceived=0x7f05000a;
+        public static final int strSent=0x7f05000b;
+        public static final int strTotal=0x7f050004;
+        public static final int strUploaded=0x7f05000e;
     }
     public static final class style {
         /** 
--- Modules/RadioMonitoringModule/src/com/acropolis/radio/module/RadioModuleActivity.java
+++ Modules/RadioMonitoringModule/src/com/acropolis/radio/module/RadioModuleActivity.java
@@ -1,11 +1,14 @@
 package com.acropolis.radio.module;
 
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 import com.acropolis.module.listener.minutes.R;
 import com.acropolis.radio.module.global.Common;
@@ -17,11 +20,26 @@ public class RadioModuleActivity extends Activity {
 
 	static Context appContext = null;
 	static Intent appIntent = null;
-	
+
 	String fetchedCallType = "";
 	String fetchedCallDuration = "";
 	String fetchedCallTimeStamp = "";
-	
+
+	//Minutes
+	TextView inValue = (TextView) this.findViewById(R.id.valueIn);
+	TextView outValue = (TextView) this.findViewById(R.id.valueOut);
+	TextView totalVoiceValue = (TextView) this.findViewById(R.id.txtVTotal);
+
+	//Messages
+	TextView rcvValue = (TextView) this.findViewById(R.id.valueRcv);
+	TextView sntValue = (TextView) this.findViewById(R.id.valueSnt);
+	TextView totalMsgValue = (TextView) this.findViewById(R.id.valueMTotal);
+
+	//Data
+	TextView downValue = (TextView) this.findViewById(R.id.valueDown);
+	TextView upValue = (TextView) this.findViewById(R.id.valueUp);
+	TextView totalDataValue = (TextView) this.findViewById(R.id.valueDTotal);
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -30,9 +48,8 @@ public class RadioModuleActivity extends Activity {
 		appIntent = this.getIntent();
 		StartMessagingEngine();
 		setContentView(R.layout.activity_minutes_module);
-		
-		
-		
+
+		fetchCurrentMonitoredValues();
 	}
 
 	@Override
@@ -69,23 +86,23 @@ public class RadioModuleActivity extends Activity {
 					Common.DUMMY_UPLOADED);
 		}
 	}
-	
+
 	public static Intent getAppIntent()
 	{
 		return appIntent;
 	}
-	
+
 	public static Context getAppContext()
 	{
 		return appContext;
 	}
-	
+
 	public void DecorateScreen()
 	{
 		Logger.Information("--->>I#"+this.getClass()+"\ndecorating-screen");
 		//TODO
 	}
-	
+
 	private boolean StartMessagingEngine()
 	{
 		Logger.Debug("--->>D#"+this.getClass().toString());
@@ -95,5 +112,68 @@ public class RadioModuleActivity extends Activity {
 				new IntentFilter("android.provider.Telephony.SMS_RECEIVED"));
 		return true;
 	}
-	
+
+	/**
+	 * Retrieve current monitored values
+	 * Accessed by onClick#refreshValues & onCreate
+	 * @return true
+	 */
+	public boolean fetchCurrentMonitoredValues()
+	{
+		/*Minutes*/
+		long incomingMin = ConvertToMinutes( 
+				Long.parseLong(
+						DBAdapter.retrieveAValue(DBConstants.INCOMINGCALL)));
+		long outgoingMin = ConvertToMinutes(
+				Long.parseLong(
+						DBAdapter.retrieveAValue(DBConstants.OUTGOINGCALL)));
+		long totalMin = incomingMin + outgoingMin;
+
+		inValue.setText(String.valueOf(incomingMin));
+		outValue.setText(String.valueOf(outgoingMin));
+		totalVoiceValue.setText(String.valueOf(totalMin));
+		
+		/*Messages*/
+		long recvMsg = Long.parseLong(DBAdapter.retrieveAValue(DBConstants.RECEIVEDMSG));
+		long sntMsg = Long.parseLong(DBAdapter.retrieveAValue(DBConstants.SENTMSG));
+		long totalMsgs = recvMsg + sntMsg;
+		
+		rcvValue.setText(String.valueOf(recvMsg));
+		sntValue.setText(String.valueOf(sntMsg));
+		totalMsgValue.setText(String.valueOf(totalMsgs));
+		
+		/*Data*/
+		long downloadData = ConvertToKB(
+				Long.parseLong(
+						DBAdapter.retrieveAValue(DBConstants.DOWNLOADED)));
+		long uploadData = ConvertToKB(
+				Long.parseLong(
+						DBAdapter.retrieveAValue(DBConstants.UPLOADED)));
+		long totalData = downloadData + uploadData;
+		
+		downValue.setText(String.valueOf(downloadData));
+		upValue.setText(String.valueOf(uploadData));
+		totalDataValue.setText(String.valueOf(totalData));
+		
+		return true;
+	}
+
+	public static long ConvertToMinutes(long seconds)
+	{
+		return TimeUnit.MINUTES.convert(seconds, TimeUnit.SECONDS);
+	}
+
+	public static long ConvertToKB(long bytes)
+	{
+		return (long) Math.ceil(bytes/(1024));
+	}
+
+	/**
+	 * R.id.bttnRefresh onClick
+	 */
+	public void refreshValues()
+	{
+		fetchCurrentMonitoredValues();
+	}
+
 }
